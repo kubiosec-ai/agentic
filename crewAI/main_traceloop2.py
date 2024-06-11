fccb431db83b5656de529f5345c2e0984fe89f05
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-06-11 20:16:50 +0200

    Create main_traceloop2.py

---
+++ crewAI/main_traceloop2.py
@@ -0,0 +1,103 @@
+import os
+from crewai import Task
+from crewai import Crew, Process
+from traceloop.sdk import Traceloop
+from traceloop.sdk.decorators import workflow
+
+
+# os.environ["SERPER_API_KEY"] = "xxxx"  # serper.dev API key
+# os.environ["OPENAI_API_KEY"] = "xxxx"
+
+
+
+from crewai import Agent
+from crewai_tools import SerperDevTool
+
+search_tool = SerperDevTool()
+
+Traceloop.init(
+  app_name="crewai_generation_service",
+  disable_batch=True,
+)
+
+
+# Creating a senior researcher agent with memory and verbose mode
+researcher = Agent(
+  role='Senior Researcher',
+  goal='Uncover groundbreaking technologies in {topic}',
+  verbose=True,
+  memory=True,
+  backstory=(
+    "Driven by curiosity, you're at the forefront of"
+    "innovation, eager to explore and share knowledge that could change"
+    "the world."
+  ),
+  tools=[search_tool],
+  allow_delegation=True
+)
+
+# Creating a writer agent with custom tools and delegation capability
+writer = Agent(
+  role='Writer',
+  goal='Narrate compelling tech stories about {topic}',
+  verbose=True,
+  memory=True,
+  backstory=(
+    "With a flair for simplifying complex topics, you craft"
+    "engaging narratives that captivate and educate, bringing new"
+    "discoveries to light in an accessible manner."
+  ),
+  tools=[search_tool],
+  allow_delegation=False
+)
+
+
+# Research task
+research_task = Task(
+  description=(
+    "Identify the next big trend in {topic}."
+    "Focus on identifying pros and cons and the overall narrative."
+    "Your final report should clearly articulate the key points,"
+    "its market opportunities, and potential risks."
+  ),
+  expected_output='A comprehensive 3 paragraphs long report on the latest AI trends.',
+  tools=[search_tool],
+  agent=researcher,
+)
+
+# Writing task with language model configuration
+write_task = Task(
+  description=(
+    "Compose an insightful article on {topic}."
+    "Focus on the latest trends and how it's impacting the industry."
+    "This article should be easy to understand, engaging, and positive."
+  ),
+  expected_output='A 4 paragraph article on {topic} advancements formatted as markdown.',
+  tools=[search_tool],
+  agent=writer,
+  async_execution=False,
+  output_file='new-blog-post5.md'  # Example of output customization
+)
+
+
+# Forming the tech-focused crew with some enhanced configurations
+crew = Crew(
+  agents=[researcher, writer],
+  tasks=[research_task, write_task],
+  process=Process.sequential,  # Optional: Sequential task execution is default
+  memory=True,
+  cache=True,
+  max_rpm=100,
+  share_crew=True
+)
+
+# Starting the task execution process with enhanced feedback
+@workflow(name="crewai_generation_service")
+def suggest_answers(topic):
+    # Call crew.kickoff with the provided topic
+    result = crew.kickoff(inputs={'topic': topic})
+    # Print the result
+    print(result)
+
+# Example usage
+suggest_answers('Belgian elections on 9th june 2024')
